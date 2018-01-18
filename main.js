72a326dc1156879f1142e9aa50965b18efc95f2c
Nick-tsoukas <nick.tsoukas101@gmail.com>
2018-01-18 11:26:58 -0700

    Adding data for charts

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ formStock.addEventListener('submit', function(e) {
   getData(myStock);
 });
 
-
 function getData(stock) {
   var url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + stock + "&apikey=CXQN168JJQ4LTUVQ";
   fetch(url)
@@ -19,20 +18,33 @@ function getData(stock) {
     })
     .then(function(data) {
       stockData = data;
-      //console.log(stockData);
       setData(stockData);
       val.value = '';
-    });
+      var keyOfData = Object.keys(stockData["Time Series (Daily)"]);
+      var arrayOfData = [];
+      keyOfData.forEach(function(current,index,array) {
+        arrayOfData.push(stockData["Time Series (Daily)"][current]['4. close']);
+      });
+      console.log(arrayOfData);
+       return arrayOfData;
+    })
+    .then(function(ar) {
+      d3.select(".chart")
+      .selectAll("div")
+        .data(ar)
+      .enter().append("div")
+        .style("width", function(d) { return d * 10 + "px"; })
+        .text(function(d) { return d; });
+    })
 }
-
 
 function setData(data) {
   var ob = data["Time Series (Daily)"];
+  var ar = []
   for (var prop in ob) {
     if (ob.hasOwnProperty(prop)) {
-      console.log(ob[prop]["1. open"]);
+      ar.push(ob[prop]["1. open"]);
     }
-
   }
   listSymbol.innerHTML = data["Meta Data"]["2. Symbol"];
 }
